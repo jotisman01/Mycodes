826b19e829c4508fad759dc7157135bf145ec294
Jotisman Banerjee <127194267+jotisman01@users.noreply.github.com>
2023-04-23 21:52:57 +0530

    Add files via upload

---
+++ Factorial code.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int factorial(int num)
+{
+    if(num==1|| num==0) {
+        return 1;
+    }
+    else{return num*factorial(num-1);
+    }
+}
+int main(){
+    int num;
+    printf("\n Enter no.");
+    scanf("%d",&num);
+    printf("\n the factorial of %d is %d",num,factorial(num));
+    return 0;
+}
