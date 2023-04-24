adb9568f78c5855561e0fd20a19c5895cdae198a
Jotisman Banerjee <127194267+jotisman01@users.noreply.github.com>
2023-04-24 20:09:16 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,22 @@
+// const array=['value1','value2','value3'];
+// let array1=["item1","item2","item3","item4","item5","item6"];
+// console.log(array1);//prints the elements in array
+// clonearray=array1.slice(0);
+// console.log(clonearray);//cloning or copying array1
+// let [var1, ,var2, ...Newarray]=array1;/*array destructuring means var1 stor item 1 by 
+// adding a spaced comma we try to skip item 3 and adding item3 in variable 2, Newarray stores 
+// remaining elements*/
+// console.log(var1);
+// console.log(var2);
+// console.log(Newarray);
+// array2=[...array1];
+// console.log(array2);
+// array3=array1.concat(array);
+// console.log(...array3);
+// const person={ name:"jotisman",
+//   age:20  , "Good skills":["Webdev","C programming"] };/*for space b/t 2 words for creating 
+//   any object element always use string property*/
+//   console.log(person);
+//   person["Gender"]="male";
+//   console.log(person);
+//   console.log(person["Good skills"]);
