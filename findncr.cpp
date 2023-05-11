9143938fa7b6de4cb454ec4c1cdd8776d1b0f20a
Jotisman Banerjee <127194267+jotisman01@users.noreply.github.com>
2023-05-11 23:37:11 +0530

    Add files via upload

---
+++ findncr.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int fact(int n){
+    if(n==0||n==1){
+        return 1;
+    }
+    else{
+  return n*fact(n-1);
+    }
+}
+int main(){
+      int n,r;
+      cin>>n>>r;
+      cout<<"ncr is \t"<<fact(n)/(fact(r)*fact(n-r));
+  
+}
