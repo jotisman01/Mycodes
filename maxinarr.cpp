6a560bf95aaec7c40e3622747dcc2f326217ba2b
Jotisman Banerjee <127194267+jotisman01@users.noreply.github.com>
2023-05-13 23:08:48 +0530

    Add files via upload

---
+++ maxinarr.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int getmax(int arry[],int n){
+      int max=INT16_MIN;
+      for(int i=0;i<n;i++){
+      if(arry[i]>max){
+       max=arry[i];
+      }
+      }
+    return max;
+}
+int main(){
+    int size;
+    cin>>size;
+    int arry[100];
+    for(int i=0;i<size;i++){
+        cin>>arry[i];
+    }  
+   cout<<"Maximum value is"<<getmax(arry,size)<<endl;
+}
