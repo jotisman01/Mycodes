d37855712bdbeac9f931c93177dfb8424cfcac40
Jotisman Banerjee <127194267+jotisman01@users.noreply.github.com>
2023-05-19 12:59:33 +0530

    Update Factorial code.c

--- Factorial code.c
+++ Factorial code.c
@@ -4,7 +4,8 @@ int factorial(int num)
     if(num==1|| num==0) {
         return 1;
     }
-    else{return num*factorial(num-1);
+    else{
+        return num*factorial(num-1);
     }
 }
 int main(){
